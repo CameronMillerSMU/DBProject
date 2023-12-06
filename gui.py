a1f7de2d032e4ef39b452142fff76dd7a5b4961f
CameronMillerSMU <78335921+CameronMillerSMU@users.noreply.github.com>
2023-12-06 12:58:46 -0600

    department query gui

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
-from queries import *  # Assuming you have your query functions in a 'queries' module
-from entry import *    # Assuming you have your entry functions in an 'entry' module
+from queries import *
+from entry import *
 
 class DatabaseGUI:
     def __init__(self, master, cursor):
@@ -17,24 +17,15 @@ class DatabaseGUI:
         self.action_var.set("Select Action")
 
         self.action_dropdown = ttk.Combobox(master, textvariable=self.action_var, values=["Entry", "Query"])
-        self.action_dropdown.pack()
+        self.action_dropdown.pack(pady=0)  # Set pady to 0
 
         self.action_button = tk.Button(master, text="Select", command=self.handle_selection)
         self.action_button.pack()
 
-        self.value_label = tk.Label(master, text="Value Returned:")
-        self.value_label.pack()
-
-        self.value_returned = tk.Label(master, text="")
-        self.value_returned.pack()
+        self.result_text = tk.Text(master, height=10, width=40)
+        self.result_text.pack()
 
     def handle_selection(self):
-        # Clear previously added widgets
-        for widget in self.option_widgets.values():
-            widget.destroy()
-
-        self.value_returned.config(text="")  # Clear the value returned label
-
         selected_action = self.action_var.get()
 
         if selected_action == "Entry":
@@ -69,11 +60,6 @@ class DatabaseGUI:
         self.option_widgets["option_button"] = option_button
 
     def handle_option(self, selected_option, selected_value):
-        # Clear previously added widgets except for the first dropdown
-        for key, widget in self.option_widgets.items():
-            if key != "option_dropdown":
-                widget.destroy()
-
         if self.action_var.get() == "Query" and selected_option == "Department":
             self.show_department_query_form(selected_value)
 
@@ -86,7 +72,7 @@ class DatabaseGUI:
         department_code_entry = tk.Entry(self.master)
         department_code_entry.pack()
 
-        execute_button = tk.Button(self.master, text="Execute", command=lambda: self.handle_department_query(selected_value, department_code_entry.get()))
+        execute_button = tk.Button(self.master, text="Execute", command=lambda: self.execute_department_query(department_code_entry.get()))
         execute_button.pack()
 
         # Keep track of added widgets for clearing later
@@ -94,16 +80,19 @@ class DatabaseGUI:
         self.option_widgets["department_code_entry"] = department_code_entry
         self.option_widgets["execute_button"] = execute_button
 
-    def handle_department_query(self, selected_value, department_code):
-        # Implement the query logic here using the entered department code
-        result = get_department(self.cursor, department_code)  # Assuming get_department is a function in your 'queries' module
-        self.display_result(result)
+    def execute_department_query(self, department_code):
+        department_data = get_department(self.cursor, department_code)
 
-    def display_result(self, result):
-        if result is not None:
-            self.value_returned.config(text=str(result))
+        if not department_data:
+            result_text = "Department does not exist. Please check department code."
         else:
-            self.value_returned.config(text="No data found.")
+            department_id, department_name, department_code = department_data
+            result_text = f"Department ID: {department_id}\nDepartment Name: {department_name}\nDepartment Code: {department_code}"
+
+        # Display result in the Text widget
+        self.result_text.delete(1.0, tk.END)  # Clear previous text
+        self.result_text.insert(tk.END, result_text)
+
 
 if __name__ == "__main__":
     root = tk.Tk()
