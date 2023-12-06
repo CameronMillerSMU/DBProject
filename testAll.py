7a876305036480fbe95a6a165f0ac9c2705f42cb
zicaldwell <zcaldwell@smu.edu>
2023-12-06 13:21:06 -0600

    updated functions

--- testAll.py
+++ testAll.py
@@ -5,22 +5,22 @@ from mysql.connector import Error
 import re
 
 
-'''General Database Functions'''
-tables = ["Program", "Department", "Faculty", "Course", "Section", 
-          "LearningObjective", "SubObjective", "CourseEval",
-          "SectionEval", "ProgramObjective"]
+# '''General Database Functions'''
+# tables = ["Program", "Department", "Faculty", "Course", "Section", 
+#           "LearningObjective", "SubObjective", "CourseEval",
+#           "SectionEval", "ProgramObjective", "ProgramCourse"]
 
 
-def clear_database(cursor, connection, tableList):
-    try:
-        cursor.execute("SET FOREIGN_KEY_CHECKS = 0")
-        for table in tableList:
-            sqlcom = "DROP TABLE IF EXISTS %s" % (table)
-            cursor.execute(sqlcom)
-        cursor.execute("SET FOREIGN_KEY_CHECKS = 1")
-    except Error as e:
-        print(f"The error '{e} occurred'")
-    connection.commit()
+# def clear_database(cursor, connection, tableList):
+#     try:
+#         cursor.execute("SET FOREIGN_KEY_CHECKS = 0")
+#         for table in tableList:
+#             sqlcom = "DROP TABLE IF EXISTS %s" % (table)
+#             cursor.execute(sqlcom)
+#         cursor.execute("SET FOREIGN_KEY_CHECKS = 1")
+#     except Error as e:
+#         print(f"The error '{e} occurred'")
+#     connection.commit()
 
 
 '''Input Handling Helper Functions'''
@@ -189,32 +189,31 @@ def enter_faculty_info(cursor, connection, fac_id, fac_name, fac_email, dept_cod
         print(f"Error inserting faculty data: {e}")
         return False
 
-def enter_course_info(cursor, connection, co_id, co_title, co_desc, dept_code):
-    if not check_dept_exists(cursor, dept_code):
-        return False
+# def enter_course_info(cursor, connection, co_id, co_title, co_desc, dept_code):
+#     if not check_dept_exists(cursor, dept_code):
+#         return False
     
-    try:
-        sqlcom = "INSERT INTO Course (CourseID, CourseTitle, CourseDescription, DepartmentCode) \
-        VALUES (%(co_id)s, %(co_title)s, %(co_desc)s, %(dept_code)s)"
-        course_data = {'co_id': co_id, 'co_title': co_title, 'co_desc': co_desc, 'dept_code': dept_code}
-#         # Insert data into the Courses table
+#     try:
 #         sqlcom = "INSERT INTO Course (CourseID, CourseTitle, CourseDescription, DepartmentCode) \
-#             VALUES (%s, %s, %s, %s)"
-#         course_data = (co_id, co_title, co_desc, dept_code)
-#         print(course_data)
-        cursor.execute(sqlcom, course_data)
-        #cursor.execute(sqlcom)
+#         VALUES (%(co_id)s, %(co_title)s, %(co_desc)s, %(dept_code)s)"
+#         course_data = {'co_id': co_id, 'co_title': co_title, 'co_desc': co_desc, 'dept_code': dept_code}
+# #         # Insert data into the Courses table
+# #         sqlcom = "INSERT INTO Course (CourseID, CourseTitle, CourseDescription, DepartmentCode) \
+# #             VALUES (%s, %s, %s, %s)"
+# #         course_data = (co_id, co_title, co_desc, dept_code)
+# #         print(course_data)
+#         cursor.execute(sqlcom, course_data)
+#         #cursor.execute(sqlcom)
 
-        # Commit the transaction
-        connection.commit()
+#         # Commit the transaction
+#         connection.commit()
 
-        print(f"Data for course '{co_title}' successfully inserted into the Courses table.")
-        return True
-    except Error as e:
-        print(f"Error inserting course data: {e}")
-        return False
+#         print(f"Data for course '{co_title}' successfully inserted into the Courses table.")
+#         return True
+#     except Error as e:
+#         print(f"Error inserting course data: {e}")
+#         return False
     
-# ENTER DATA INTO TABLES
 def enter_course_data(cursor, connector, course_id, course_title, c_description, dept_code):
     # check if the dept exists - if not, return error
     if not check_dept_exists(cursor, dept_code):
@@ -222,8 +221,12 @@ def enter_course_data(cursor, connector, course_id, course_title, c_description,
     
     try:
         #insert data into Course table
-        cursor.execute("INSERT INTO Course (CourseID, CourseTitle, CourseDescription, DepartmentCode) VALUES (?,?,?,?)",
-                   course_id, course_title, c_description, dept_code)
+        command = "INSERT INTO Course (CourseID, CourseTitle, CourseDescription, DepartmentCode) \
+            VALUES (%s, %s, %s, %s)"
+        course_values = (course_id, course_title, c_description, dept_code)
+        # print(command, course_values)
+        cursor.execute(command, course_values)
+
         # commit changes to the database 
         connector.commit()
         return True
@@ -240,8 +243,12 @@ def enter_section_data(cursor, connector, course_id, semester, year, f_id, stude
 
     try:
         #insert data into Sections table
-        cursor.execute("INSERT INTO CourseSections (CourseID, SemesterName, CourseYear, FacultyID, StudentsEnrolled) VALUES (?,?,?,?,?)",
-                       course_id, semester, year, f_id, students_enrolled)
+        command = "INSERT INTO Section (CourseID, SemesterName, CourseYear, FacultyID, StudentsEnrolled) \
+            VALUES (%s, %s, %s, %s, %s)"
+        section_values = (course_id, semester, year, f_id, students_enrolled)
+        # print(command, section_values)
+        cursor.execute(command, section_values)
+
         # commit changes to the database 
         connector.commit()
         return True
@@ -252,8 +259,12 @@ def enter_section_data(cursor, connector, course_id, semester, year, f_id, stude
 def enter_learningObjective_data(cursor, connector, obj_code, obj_description):
     try:
         #insert data into Learning Objectives table
-        cursor.execute("INSERT INTO LearningObjectives (ObjectiveCode, ObjectiveDescription) VALUES (?,?)",
-                       obj_code, obj_description)
+        command = "INSERT INTO LearningObjective (ObjectiveCode, ObjectiveDescription) \
+            VALUES (%s, %s)"
+        lo_values = (obj_code, obj_description)
+        # print(command, lo_values)
+        cursor.execute(command, lo_values)
+
         # commit changes to the database 
         connector.commit()
         return True
@@ -268,13 +279,17 @@ def enter_subObjective_data(cursor, connector, subObj_code, obj_code, subObj_des
     
     try:
         #insert data into Learning Objective table
-        cursor.execute("INSERT INTO LearningObjectives (SubObjectiveCode, SubObjectiveDescription, ObjectiveCode) VALUES (?,?,?)",
-                       subObj_code, obj_code, subObj_description)
+        command = "INSERT INTO SubObjective (SubObjectiveCode, SubObjectiveDescription, ObjectiveCode) \
+            VALUES (%s, %s, %s)"
+        subObj_values = (subObj_code, subObj_description, obj_code)
+        # print(command, subObj_values)
+        cursor.execute(command, subObj_values)
+
         # commit changes to the database 
         connector.commit()
         return True
     except Error as e:
-        print(f"Error inserting Learning Objectives data: {e}")
+        print(f"Error inserting Sub-Objectives data: {e}")
         return False
 
 
@@ -294,7 +309,7 @@ def assign_course_to_program(cursor, connector, course_id, prog_name):
         connector.commit()
         return True
     except Error as e:
-        print(f"Error inserting Learning Objectives data: {e}")
+        print(f"Error inserting Course to Program: {e}")
         return False
     
 '''Assigning learning (sub)objectives to (course, program) pairs (remember, a course can
@@ -316,10 +331,29 @@ def assign_obj_to_course(cursor, connection, co_id, prog_name, obj_code):
             connection.commit()
             return True
         except Error as e:
-            print(f"Error inserting Program Course data: {e}")
+            print(f"Error assigning objective to course data: {e}")
             return False
     return False
 
+def get_section_id(cursor, connection, c_id, semester, year):
+    try:
+        command = "SELECT SectionID, FacultyID FROM Section WHERE CourseID = '%s' AND \
+            SemesterName = '%s' AND CourseYear = %d" % (c_id, semester, year) 
+        cursor.execute(command)
+        return cursor.fetchall()
+    except Error as e:
+        print(f"Error getting Section ID data: {e}")
+    
+def enter_section_eval_results(cursor, connction, section_id, course_id, prog_name, obj_code, stud_met):
+    try:
+        command = "UPDATE SectionEval set StudentsMetObj = %d WHERE ObjectionCode = '%s' \
+            AND SectionID = '%s' AND CourseID = '%s' AND ProgramName = '%s'"
+        section_eval_values = (stud_met, obj_code, section_id, course_id, prog_name)
+        cursor.execute(command, section_eval_values)
+        return True
+    except Error as e:
+        print(f"Error entering section evaluation results: {e}")
+        return False
 
 '''Input Handling Functions'''
 
@@ -352,21 +386,27 @@ def handle_faculty_entry(cursor, connection, fac_id, fac_name, fac_email, dept_c
         return "Email is not valid. Please try again."
 
     
-def handle_course_entry(cursor, connection, co_id, co_title, co_desc, dept_code):
-    if not check_dept_code(dept_code) and check_course_id(co_id):
-        return "Department Code or Course ID not valid"
+# def handle_course_entry(cursor, connection, co_id, co_title, co_desc, dept_code):
+#     if not check_dept_code(dept_code) and check_course_id(co_id):
+#         return "Department Code or Course ID not valid"
     
-    if enter_course_info(cursor, connection, str(co_id), str(co_title), str(co_desc), str(dept_code)):
-        return "Course Info Stored Successfully"
+#     if enter_course_info(cursor, connection, str(co_id), str(co_title), str(co_desc), str(dept_code)):
+#         return "Course Info Stored Successfully"
     
 # Functions to Handle User Input
 def handle_course_entry(cursor, connector, course_id, course_title, c_description, dept_code):
+    if not check_dept_code(dept_code) and check_course_id(course_id):
+        return "Department Code or Course ID not valid"
+    
     if enter_course_data(cursor, connector, str(course_id), str(course_title), str(c_description), str(dept_code)):
         return "Course data entered successfully."
     else: 
         return "Error entering course data. Please try again."
     
 def handle_section_entry(cursor, connector, course_id, semester, year, f_id, students_enrolled):
+    if not check_course_id(course_id):
+        return "Course ID not valid"
+
     if enter_section_data(cursor, connector, str(course_id), str(semester), year, str(f_id), int(students_enrolled)):
         return "Section data entered successfully."
     else: 
@@ -381,34 +421,56 @@ def handle_learningObjective_entry(cursor, connector, obj_code, obj_description)
 def handle_subObjective_entry(cursor, connector, obj_code, subObj_description):
     # create the sub-objective code 
     subObj_code = create_subObj_code(cursor, obj_code)
+    print(subObj_code)
 
     if enter_subObjective_data(cursor, connector, str(subObj_code), str(obj_code), str(subObj_description)):
         return "Sub-objective data entered successfully."
     else: 
         return "Error entering Sub-objective data. Please try again."
     
+def handle_courseProgram_assignment(cursor, connector, course_id, prog_name):
+    if assign_course_to_program(cursor, connector, course_id, prog_name):
+       return "Course assigned to program successfully."
+    else: 
+        return "Error assigning course to program."
+    
+def handle_objCourse_assignment(cursor, connection, co_id, prog_name, obj_code):
+    if assign_obj_to_course(cursor, connection, co_id, prog_name, obj_code):
+        return "Objective assigned to course successfully"
+    else:
+        return "Error assigning objective to course."
 
-
+def handle_sectionEval_entry(cursor, connction, section_id, course_id, prog_name, obj_code, stud_met):
+    if enter_section_eval_results(cursor, connction, section_id, course_id, prog_name, obj_code, stud_met):
+        return "Entered section evaluation results successfully."
+    else:
+        return "Error entering section evaluation results."
 
 def driver():
     
     """CHANGE THIS TO RUN"""
-    dbConn = create_connection("localhost", "root", "123456", "progDB")
-    print(dbConn)
+    dbConn = create_connection("localhost", "root", "beepboop", "progDB")
+    # print(dbConn)
     cursor = dbConn.cursor()
-    print(cursor)
+    # print(cursor)
     create_database(cursor, "progDB")
-    
+    clear_database(cursor, dbConn, tables)
     
     create_tables_from_file(cursor, "test_schema.sql", dbConn)
     
 
     
-    print(handle_department_entry(cursor, dbConn, "ABCD", "ABCD Department"))
-    print(handle_faculty_entry(cursor, dbConn, "1111111", "Test Falc", "faltest@gmail.com", "ABCD", "Adjunct"))
-    print(handle_program_entry(cursor, dbConn, "Test 2 Program", "1111111", "ABCD"))
-    print(handle_faculty_entry(cursor, dbConn, "1111113", "Test Falc 3", "faltest3@gmail.com", "ABCD", "boop"))
+    print(handle_department_entry(cursor, dbConn, "ABCD", "ABCD Department")) # good
+    print(handle_faculty_entry(cursor, dbConn, "1111111", "Test Falc", "faltest@gmail.com", "ABCD", "Adjunct")) #good
+    print(handle_program_entry(cursor, dbConn, "Test 2 Program", "1111111", "ABCD")) # good
+    print(handle_faculty_entry(cursor, dbConn, "1111113", "Test Falc 3", "faltest3@gmail.com", "ABCD", "boop")) #fail
     #handle_course_entry(cursor, dbConn, "CS1111", "Test Course", "hello this course is for dunces like you", "0000")
+    print(handle_course_entry(cursor, dbConn, "ABCD0001", "Learning Abc's", "learn something", "ABCD"))
+    print(handle_section_entry(cursor, dbConn, "ABCD0001", "Fall", 2023, "1111111", 20))
+    print(handle_learningObjective_entry(cursor, dbConn, "LO1", "here is the decription"))
+    print(handle_subObjective_entry(cursor, dbConn, "LO1", "here is the sub objective description"))
+    print(handle_courseProgram_assignment(cursor, dbConn, "ABCD0001", "Test 2 Program"))
+
 
 
 driver()
