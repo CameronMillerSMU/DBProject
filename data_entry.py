d119780f26ab51375efc1a5be6f3369772222370
zicaldwell <zcaldwell@smu.edu>
2023-12-05 19:27:52 -0600

    updated test schema

--- data_entry.py
+++ data_entry.py
@@ -1,7 +1,7 @@
 from project import *
 
-db = create_connection("localhost", "root", "beepboop", "DBprog")
-cursor = db.cursor()
+db_conn = create_connection("localhost", "root", "beepboop", "DBprog")
+cursor = db_conn.cursor()
 create_database(cursor, "DBprog")
 
 #todo: create the tables from file 
@@ -22,5 +22,33 @@ def execute_scripts_from_file(filename):
 
 execute_scripts_from_file("test_schema.sql")
 
+# TODO: data entry for Courses, Sections & Learning objectives / sub-objectives
 
+def enter_course_data(cursor, db_conn, c_id, c_title, c_description, dp_code):
+    try:
+        #insert data into Courses table
+        cursor.execute("INSERT INTO COURSES (CourseID, CourseTitle, CourseDescription, DepartmentCode) VALUES (?,?,?,?)",
+                   c_id, c_title, c_description, dp_code)
+        # commit changes to the database 
+        db_conn.commit()
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+def enter_section_data(cursor, db_conn):
+    try:
+        #insert data into Sections table
+        cursor.execute("")
+        # commit changes to the database 
+        db_conn.commit()
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+def enter_learningObjectives_data(cursor, db_conn):
+    try:
+        #insert data into Learning Objectives table
+        cursor.execute("")
+        # commit changes to the database 
+        db_conn.commit()
+    except Error as e:
+        print(f"The error '{e}' occurred")
  
