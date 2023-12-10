f62e329a61f409cd2540cb5f111658aeeacba405
CameronMillerSMU <78335921+CameronMillerSMU@users.noreply.github.com>
2023-12-09 18:15:01 -0600

    entries on execute

--- gui.py
+++ gui.py
@@ -25,11 +25,19 @@ class DatabaseGUI:
         query_button.pack()
 
     def show_entry_options(self):
-        entry_options = ["Program", "Department", "Faculty", "Course", "Section", "LearningObjective", "Evaluation", "Assign Course", "Assign Learning Objective"]
+        # destroy the previous selected option label
+        if hasattr(self, 'selected_option_label'):
+            self.selected_option_label.destroy()
+
+        entry_options = ["Program", "Department", "Faculty", "Course", "Section", "Learning Objective"]
         self.action_var.set("Entry")
         self.show_options(entry_options)
 
     def show_query_options(self):
+        # destroy the previous selected option label
+        if hasattr(self, 'selected_option_label'):
+            self.selected_option_label.destroy()
+
         query_options = ["Department", "Program", "Semester", "Academic Year"]
         self.action_var.set("Query")
         self.show_options(query_options)
@@ -64,6 +72,10 @@ class DatabaseGUI:
             widget.destroy()
 
     def handle_option(self, selected_option, selected_value):
+        # show the selected option above the form
+        self.selected_option_label = tk.Label(self.master, text=f"Selected Option: {selected_option}")
+        self.selected_option_label.pack()
+
         # Queries
         if self.action_var.get() == "Query" and selected_option == "Program":
             self.show_program_query_form(selected_value)
@@ -87,7 +99,7 @@ class DatabaseGUI:
             self.show_course_entry_form(selected_value)
         elif self.action_var.get() == "Entry" and selected_option == "Section":
             self.show_section_entry_form(selected_value)
-        elif self.action_var.get() == "Entry" and selected_option == "LearningObjective":
+        elif self.action_var.get() == "Entry" and selected_option == "Learning Objective":
             self.show_learning_objective_entry_form(selected_value)
 
     def show_program_query_form(self, selected_value):
@@ -292,53 +304,56 @@ class DatabaseGUI:
         self.option_widgets["execute_button"] = execute_button
         
     def show_section_entry_form(self, selected_value):
+        # course_id, semester, year, faculty_id, students_enrolled
         self.destroy_option_widgets()
 
-        course_id_label = tk.Label(self.master, text="Course ID:")
+        course_id_label = tk.Label(self.master, text=f"{selected_value} ID:")
         course_id_label.pack()
 
         course_id_entry = tk.Entry(self.master)
         course_id_entry.pack()
 
-        course_title_label = tk.Label(self.master, text="Course Title:")
-        course_title_label.pack()
+        semester_label = tk.Label(self.master, text=f"{selected_value} Semester:")
+        semester_label.pack()
 
-        course_title_entry = tk.Entry(self.master)
-        course_title_entry.pack()
+        semester_entry = tk.Entry(self.master)
+        semester_entry.pack()
 
-        course_description_label = tk.Label(self.master, text="Course Description:")
-        course_description_label.pack()
+        year_label = tk.Label(self.master, text=f"{selected_value} Year:")
+        year_label.pack()
 
-        course_description_entry = tk.Entry(self.master)
-        course_description_entry.pack()
+        year_entry = tk.Entry(self.master)
+        year_entry.pack()
 
-        department_code_label = tk.Label(self.master, text="Department Code:")
-        department_code_label.pack()
+        faculty_id_label = tk.Label(self.master, text=f"{selected_value} Faculty ID:")
+        faculty_id_label.pack()
 
-        department_code_entry = tk.Entry(self.master)
-        department_code_entry.pack()
+        faculty_id_entry = tk.Entry(self.master)
+        faculty_id_entry.pack()
 
-        execute_button = tk.Button(self.master, text="Execute", command=lambda: self.execute_section_entry(course_id_entry.get(), course_title_entry.get(), course_description_entry.get(), department_code_entry.get()))
+        students_enrolled_label = tk.Label(self.master, text=f"{selected_value} Students Enrolled:")
+        students_enrolled_label.pack()
+
+        students_enrolled_entry = tk.Entry(self.master)
+        students_enrolled_entry.pack()
+
+        execute_button = tk.Button(self.master, text="Execute", command=lambda: self.execute_section_entry(course_id_entry.get(), semester_entry.get(), year_entry.get(), faculty_id_entry.get(), students_enrolled_entry.get()))
         execute_button.pack()
 
         self.option_widgets["course_id_label"] = course_id_label
         self.option_widgets["course_id_entry"] = course_id_entry
-        self.option_widgets["course_title_label"] = course_title_label
-        self.option_widgets["course_title_entry"] = course_title_entry
-        self.option_widgets["course_description_label"] = course_description_label
-        self.option_widgets["course_description_entry"] = course_description_entry
-        self.option_widgets["department_code_label"] = department_code_label
-        self.option_widgets["department_code_entry"] = department_code_entry
+        self.option_widgets["semester_label"] = semester_label
+        self.option_widgets["semester_entry"] = semester_entry
+        self.option_widgets["year_label"] = year_label
+        self.option_widgets["year_entry"] = year_entry
+        self.option_widgets["faculty_id_label"] = faculty_id_label
+        self.option_widgets["faculty_id_entry"] = faculty_id_entry
+        self.option_widgets["students_enrolled_label"] = students_enrolled_label
+        self.option_widgets["students_enrolled_entry"] = students_enrolled_entry
         self.option_widgets["execute_button"] = execute_button
 
     def show_learning_objective_entry_form(self, selected_value):
         self.destroy_option_widgets()
-
-        objective_name_label = tk.Label(self.master, text=f"{selected_value} Name:")
-        objective_name_label.pack()
-
-        objective_name_entry = tk.Entry(self.master)
-        objective_name_entry.pack()
 
         objective_code_label = tk.Label(self.master, text=f"{selected_value} Code:")
         objective_code_label.pack()
@@ -346,14 +361,31 @@ class DatabaseGUI:
         objective_code_entry = tk.Entry(self.master)
         objective_code_entry.pack()
 
-        execute_button = tk.Button(self.master, text="Execute", command=lambda: self.execute_learning_objective_entry(objective_name_entry.get(), objective_code_entry.get()))
+        objective_description_label = tk.Label(self.master, text=f"{selected_value} Description:")
+        objective_description_label.pack()
+
+        objective_description_entry = tk.Entry(self.master)
+        objective_description_entry.pack()
+
+        execute_button = tk.Button(self.master, text="Execute", command=lambda: self.execute_learning_objective_entry(objective_code_entry.get(), objective_description_entry.get()))
         execute_button.pack()
 
-        self.option_widgets["objective_name_label"] = objective_name_label
-        self.option_widgets["objective_name_entry"] = objective_name_entry
         self.option_widgets["objective_code_label"] = objective_code_label
         self.option_widgets["objective_code_entry"] = objective_code_entry
+        self.option_widgets["objective_description_label"] = objective_description_label
+        self.option_widgets["objective_description_entry"] = objective_description_entry
         self.option_widgets["execute_button"] = execute_button
+
+
+    def execute_program_query(self, program_name):
+        program_data = get_program(self.cursor, program_name)
+
+        if not program_data:
+            result_text = "Program does not exist. Please check program name."
+        else:
+            program_id, program_name, program_coordinator_id, department_code = program_data
+            result_text = f"Program ID: {program_id}\nProgram Name: {program_name}\nProgram Coordinator ID: {program_coordinator_id}\nDepartment Code: {department_code}"
+        print(result_text)
 
     def execute_department_query(self, department_code):
         department_data = get_department(self.cursor, department_code)
@@ -363,10 +395,51 @@ class DatabaseGUI:
         else:
             department_id, department_name, department_code = department_data
             result_text = f"Department ID: {department_id}\nDepartment Name: {department_name}\nDepartment Code: {department_code}"
-
-        # Display result in the console (you can customize this as needed)
         print(result_text)
 
+    def execute_semester_query(self, semester_name):
+        semester_data = get_semester(self.cursor, semester_name)
+
+        if not semester_data:
+            result_text = "Semester does not exist. Please check semester name."
+        else:
+            semester_id, semester_name = semester_data
+            result_text = f"Semester ID: {semester_id}\nSemester Name: {semester_name}"
+        print(result_text)
+
+    def execute_academic_year_query(self, academic_year_name):
+        academic_year_data = get_academic_year(self.cursor, academic_year_name)
+
+        if not academic_year_data:
+            result_text = "Academic Year does not exist. Please check academic year name."
+        else:
+            academic_year_id, academic_year_name = academic_year_data
+            result_text = f"Academic Year ID: {academic_year_id}\nAcademic Year Name: {academic_year_name}"
+        print(result_text)
+
+    def execute_program_entry(self, program_name, program_coordinator_id, department_code):
+        result_text = handle_program_entry(self.cursor, self.connector, program_name, program_coordinator_id, department_code)
+        print(result_text)
+
+    def execute_department_entry(self, department_name, department_code):
+        result_text = handle_department_entry(self.cursor, self.connector, department_code, department_name)
+        print(result_text)
+
+    def execute_faculty_entry(self, faculty_name, faculty_email, department_code, faculty_rank):
+        result_text = handle_faculty_entry(self.cursor, self.connector, faculty_name, faculty_email, department_code, faculty_rank)
+        print(result_text)
+
+    def execute_course_entry(self, course_id, course_title, course_description, department_code):
+        result_text = handle_course_entry(self.cursor, self.connector, course_id, course_title, course_description, department_code)
+        print(result_text)
+
+    def execute_section_entry(self, course_id, semester, year, faculty_id, students_enrolled):
+        result_text = handle_section_entry(self.cursor, self.connector, course_id, semester, year, faculty_id, students_enrolled)
+        print(result_text)
+
+    def execute_learning_objective_entry(self, objective_code, objective_description):
+        result_text = handle_learningObjective_entry(self.cursor, self.connector, objective_code, objective_description)
+        print(result_text)
 
 if __name__ == "__main__":
     root = tk.Tk()
